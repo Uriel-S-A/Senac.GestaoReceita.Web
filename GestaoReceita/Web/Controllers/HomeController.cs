44d473e6b812af205c081611ad3e44d4cbea83fc
usilva <usilva01@universalleaf.com>
2023-08-23 13:07:12 -0300

    teste push

--- GestaoReceita/Web/Controllers/HomeController.cs
+++ GestaoReceita/Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "asdfasdfasdfasdfsadfasdf";
 
             return View();
         }
