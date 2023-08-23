84fdf4ddece98cb852cb1dff2f07922d10f1daa0
Gabriel Schvengber <gabrielcostaschvengber@gmail.com>
2023-08-23 20:42:57 -0300

    teste do gabriel

--- GestaoReceita/Web/Controllers/HomeController.cs
+++ GestaoReceita/Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "asdfasdfasdfadfasdfasdf";
+            ViewBag.Message = "agora o gabriel fez um ajuste";
 
             return View();
         }
