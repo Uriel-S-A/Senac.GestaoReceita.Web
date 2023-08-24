2cb0ae7151aff0494f836b804a24a62d6d1436e0
Daniel <danielkloh@gmail.com>
2023-08-23 21:36:22 -0300

    Comentario

---
+++ GestaoReceita/Web/Scripts/pesquisarItem.js
@@ -0,0 +1,17 @@
+﻿window.onload = function () {
+    var url = "/Empresa/Pesquisar";
+    var json = "";//$("#inputCNPJ").val();
+
+    $.ajax({
+        method: 'POST',
+        url: url,
+        data: { inputCNPJ: "teste" },
+        success: (e) => {
+            console.log(e)
+        },
+        error: (e) => {
+            console.log('nao foi')
+        }
+    })
+
+}
