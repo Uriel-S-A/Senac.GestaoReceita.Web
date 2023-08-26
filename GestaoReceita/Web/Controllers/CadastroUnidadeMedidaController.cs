a5df472e4077b9eed62e233bd9cbac4bb2092d66
Erick <3r1ckamorim@gmail.com>
2023-08-25 22:08:21 -0300

    Criação Telas e Models, ajustes templates, e inicio de javascript

---
+++ GestaoReceita/Web/Controllers/CadastroUnidadeMedidaController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+using Web.Model.CadastroUnidadeMedida;
+using System.Collections.Generic;
+
+namespace Web.Controllers
+{
+    public class CadastroUnidadeMedidaController : Controller
+    {
+        // GET: CadastroUnidadeMedida
+        public ActionResult Index()
+        {
+            //chamada da api
+            var listaUnidadeMedida = new List<UnidadeMedida>()
+            {
+                new UnidadeMedida()
+                {
+                    Id = 1,
+                    Descricao = "Kilo",
+                    Sigla = "Kg"
+                }   ,            
+                new UnidadeMedida()
+                {
+                    Id = 2,
+                    Descricao = "Litros",
+                    Sigla = "Lt"
+                }  ,             
+                new UnidadeMedida()
+                {
+                    Id = 1,
+                    Descricao = "Kilo",
+                    Sigla = "Kg"
+                } ,              
+                new UnidadeMedida()
+                {
+                    Id = 1,
+                    Descricao = "Kilo",
+                    Sigla = "Kg"
+                }
+            };
+
+            var indexViewModel = new CadastrarUnidadeMedidaViewModel()
+            {
+                listaUnidadeMedida = listaUnidadeMedida
+            };
+            
+            return View(indexViewModel);
+        }
+
+        public ActionResult PersistirUnidadeMedida(int? id, string descricao, string sigla)
+        {
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
