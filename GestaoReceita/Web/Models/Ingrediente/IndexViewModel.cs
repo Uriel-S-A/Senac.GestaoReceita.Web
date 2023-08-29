051c7bdba8bd44bdf93d0bf240b627acc7daba7b
Igor <igorhgassen@gmail.com>
2023-08-29 11:12:14 -0300

    GetDadosEmpresa adicionado

--- GestaoReceita/Web/Models/Ingrediente/IndexViewModel.cs
+++ GestaoReceita/Web/Models/Ingrediente/IndexViewModel.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Web.Models.Ingrediente;
 
 namespace Web.Models
 {
     public class IndexViewModel
     {
         public List<DadosIngrediente> listaIngredientesCadastrados { get; set; }
-        //public List<Empresas> listaDadosEmpresa { get; set; }
+        public List<DadosEmpresa> listaDadosEmpresa { get; set; }
         //public List<UnidadeMedidas> listaDadosUnidadeMedida { get; set; }
 
         public string erros { get; set; }
